835004afda54d8bb33d8a6b43b39227d609a569e
Porramet Promsang <40641596+justpeeter@users.noreply.github.com>
2022-08-03 02:26:50 +0700

    Update logs_compare.py

--- logs_compare.py
+++ logs_compare.py
@@ -4,9 +4,10 @@ import sys
 import pandas as pd
 import numpy as np
 import re
+import datetime
 
 def getLogs(folderDir):
-	returnDF = pd.DataFrame(columns = ['name', 'record', 'duration', 'error'])
+	returnDF = pd.DataFrame(columns = ['name', 'date', 'record', 'duration', 'error'])
 
 	for filename in os.listdir(folderDir):
 		logFile = os.path.join(folderDir, filename)
@@ -14,6 +15,8 @@ def getLogs(folderDir):
 			rowRecord = ""
 			rowDuration = ""
 			rowError = "" 
+			dt_m = os.path.getmtime(logFile)
+			rowModDate = datetime.datetime.fromtimestamp(dt_m).strftime("%Y-%m-%d")
 			with open(logFile) as file:
 				for line in file:
 					if 'rows copied' in line.lower():
@@ -24,7 +27,7 @@ def getLogs(folderDir):
 						if line not in rowError:
 							rowError = rowError+line
 
-			df = pd.DataFrame({'name': [filename.replace('.log','')], 'record': [rowRecord], 'duration': [rowDuration], 'error':[rowError]})
+			df = pd.DataFrame({'name': [filename.replace('.log','')], 'date': [rowModDate], 'record': [rowRecord], 'duration': [rowDuration], 'error':[rowError]})
 			returnDF = pd.concat([returnDF,df],ignore_index=True)
 	return returnDF
 
@@ -37,7 +40,7 @@ if __name__ == "__main__":
 	parser.add_argument('-e','--exportDir', type=str, required=True)
 	parser.add_argument('-i','--importDir', type=str, required=True)
 	parser.add_argument('-o','--outputDir', type=str, default=".")
-	parser.add_argument('-n','--outputName', type=str, default="compare_logs.csv")
+	parser.add_argument('-n','--outputName', type=str, default="name_default")
 	flags = parser.parse_args()
 
 	if os.path.isdir(flags.exportDir) == False:
@@ -51,6 +54,11 @@ if __name__ == "__main__":
 	if checkArgs == False:
 		sys.exit()  
 
+	if flags.outputName == "name_default":
+		fileOutput = re.search(r"\/*.*\/*(.*)_export",flags.exportDir).group(1)
+	else:
+		fileOutput = flags.outputName
+
 	exportDF = getLogs(flags.exportDir)
 	importDF = getLogs(flags.importDir)
 
@@ -58,4 +66,5 @@ if __name__ == "__main__":
 	finalDF['match'] = np.where(finalDF['record_export'] == finalDF['record_import'],'true','false')
 	finalDF = finalDF.sort_values('name')
 
-	finalDF.to_csv(f"{flags.outputDir}/{flags.outputName}".replace('//','/'), index=False)
+	print(f"output::: {flags.outputDir}/{fileOutput}.csv")
+	finalDF.to_csv(f"{flags.outputDir}/{fileOutput}.csv".replace('//','/'), index=False)
